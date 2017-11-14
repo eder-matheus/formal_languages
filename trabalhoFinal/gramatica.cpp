310b26ddb2e85e5b6edcb1f811c7c6fca30f09ca
eder_matheus <eder.matheus.monteiro@gmail.com>
2017-11-14 14:45:29 -0200

    Sistema de leitura da gramatica em andamento

--- trabalhoFinal/gramatica.cpp
+++ trabalhoFinal/gramatica.cpp
@@ -5,8 +5,62 @@ void le_gramatica(std::string arquivo, GRAMATICA& gramatica) {
 
 	entrada.open(arquivo);
 
-	for (std::string linha; std::getline(entrada, linha);) {
-		std::cout << linha << "\n";
+	std::string linha;
+	std::string leitura_atual;
+	std::string terminal;
+	std::string variavel;
+	std::string inicial;
+	std::string regra_var;
+	std::string regra_prod;
+
+	int contador;
+
+	while (!entrada.eof()) {
+		std::getline(entrada, linha);
+
+		if (entrada.eof()) {
+			continue;
+		}
+
+		if (linha.find("#Terminais", 0) == 0) {
+			leitura_atual = "terminais";
+			continue;
+		}
+
+		if (linha.find("#Variaveis", 0) == 0) {
+			leitura_atual = "variaveis";
+			continue;
+		}
+
+		if (linha.find("#Inicial", 0) == 0) {
+			leitura_atual = "inicial";
+			continue;
+		}
+
+		if (linha.find("#Regras", 0) == 0) {
+			leitura_atual = "regras";
+			continue;
+		}
+
+		if (leitura_atual.compare("terminais") == 0) {
+			terminal = linha[linha.find("[", 0) + 2];
+			std::cout << terminal << "\n";
+		}
+
+		if (leitura_atual.compare("variaveis") == 0) {
+			variavel = linha[linha.find("[", 0) + 2];
+			std::cout << variavel << "\n";
+		}
+
+		if (leitura_atual.compare("inicial") == 0) {
+			inicial = linha[linha.find("[", 0) + 2];
+			std::cout << inicial << "\n";
+		}
+
+		if (leitura_atual.compare("regras") == 0) {
+			regra_var = linha[linha.find("[", 0) + 2];
+			std::cout << regra_var << "\n";
+		}
 	}
 
 	entrada.close();
--- trabalhoFinal/regras.h
+++ trabalhoFinal/regras.h
@@ -8,8 +8,9 @@
 #define REGRAS_H
 
 typedef struct {
-    std::string variavel;
-    std::vector<std::string> cadeia_simbolos;
+    std::string variavel; // A variavel poderia ser um char, mas ainda não temos certeza se o nome da variavel possui somente um caractere
+    std::vector<std::vector<std::string>> cadeia_simbolos; // A cadeia de simbolos representa as produções que uma variável tem
+                                                          // Como uma produção pode ter mais de um simbolo, cada produção também é um vector
 } REGRA;
 
 #endif /* REGRAS_H */
