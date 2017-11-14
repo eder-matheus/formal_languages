e6a67a97c946f83902784acbcc1c4ef2c23033b6
eder_matheus <eder.matheus.monteiro@gmail.com>
2017-11-14 17:30:04 -0200

    Sistema de leitura da gramatica em andamento

--- trabalhoFinal/gramatica.cpp
+++ trabalhoFinal/gramatica.cpp
@@ -43,21 +43,53 @@ void le_gramatica(std::string arquivo, GRAMATICA& gramatica) {
 		}
 
 		if (leitura_atual.compare("terminais") == 0) {
-			terminal = linha[linha.find("[", 0) + 2];
+			for (int i = linha.find("[", 0) + 2; i < linha.size(); i++) {
+				std::string caractere;
+				caractere.push_back(linha[i]);
+				if (caractere.compare(" ") == 0 || caractere.compare("]") == 0) {
+					break;
+				}
+				terminal.push_back(linha[i]);
+			}
 			std::cout << terminal << "\n";
+			terminal.erase();
 		}
 
 		if (leitura_atual.compare("variaveis") == 0) {
-			variavel = linha[linha.find("[", 0) + 2];
+			for (int i = linha.find("[", 0) + 2; i < linha.size(); i++) {
+				std::string caractere;
+				caractere.push_back(linha[i]);
+				if (caractere.compare(" ") == 0 || caractere.compare("]") == 0) {
+					break;
+				}
+				variavel.push_back(linha[i]);
+			}
 			std::cout << variavel << "\n";
+			variavel.erase();
 		}
 
 		if (leitura_atual.compare("inicial") == 0) {
-			inicial = linha[linha.find("[", 0) + 2];
+			for (int i = linha.find("[", 0) + 2; i < linha.size(); i++) {
+				std::string caractere;
+				caractere.push_back(linha[i]);
+				if (caractere.compare(" ") == 0 || caractere.compare("]") == 0) {
+					break;
+				}
+				inicial.push_back(linha[i]);
+			}
 			std::cout << inicial << "\n";
+			inicial.erase();
 		}
 
 		if (leitura_atual.compare("regras") == 0) {
+			for (int i = linha.find("[", 0) + 2; i < linha.size(); i++) {
+				std::string caractere;
+				caractere.push_back(linha[i]);
+				if (caractere.compare(" ") == 0 || caractere.compare("]") == 0) {
+					break;
+				}
+				regra_var.push_back(linha[i]);
+			}
 			regra_var = linha[linha.find("[", 0) + 2];
 			std::cout << regra_var << "\n";
 		}
