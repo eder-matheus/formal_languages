cb54ed26df353890401370a3181a729d4f3ac5b2
eder_matheus <eder.matheus.monteiro@gmail.com>
2017-12-17 13:41:47 -0200

    Arvores de derivacao implementadas

--- trabalhoFinal/early.cpp
+++ trabalhoFinal/early.cpp
@@ -80,6 +80,85 @@ void avancaMarcador(std::vector<std::string> &producao, int conjunto) {
 
 // -----------------------------------------------------------------------------
 
+int encontraReducao(std::string const &variavel, std::vector<std::vector<REGRA>> const &Dr, std::vector<REGRA> const &Di) {
+	int marcador;
+	int s = 0; // Variavel usada para indicar o ponteiro da producao reduzida
+
+	for (int i = 0; i < Dr.size(); i++)
+		for (int j = 0; j < Dr[i].size(); j++)
+			s++;
+
+	for (int k = 0; k < Di.size(); k++) {
+		if (variavel.compare(Di[k].variavel) == 0) {
+			marcador = encontraMarcador(Di[k].cadeia_simbolos);
+			if ((int) Di[k].cadeia_simbolos[marcador + 1][0] == 47) {
+				return s;
+			}
+		}
+		s++;
+	}
+}
+
+// -----------------------------------------------------------------------------
+
+int encontraReducaoAnterior(std::string const &variavel, std::vector<std::vector<REGRA>> const &Dr, std::vector<REGRA> const &Di) {
+	int marcador;
+	int s = 0;
+
+	for (int i = 0; i < Dr.size(); i++) {
+		for (int j = 0; j < Dr[i].size(); j++) {
+			if (variavel.compare(Dr[i][j].variavel) == 0) {
+				marcador = encontraMarcador(Dr[i][j].cadeia_simbolos);
+				if ((int) Dr[i][j].cadeia_simbolos[marcador + 1][0] == 47) {
+					return s;
+				}
+			}
+			s++;
+		}
+	}
+
+	for (int k = 0; k < Di.size(); k++) {
+		if (variavel.compare(Di[k].variavel) == 0) {
+			marcador = encontraMarcador(Di[k].cadeia_simbolos);
+			if ((int) Di[k].cadeia_simbolos[marcador + 1][0] == 47) {
+				return s;
+			}
+		}
+		s++;
+	}
+}
+
+// -----------------------------------------------------------------------------
+
+void encontraPonteiros(std::vector<std::vector<REGRA>> const &Dr, std::vector<REGRA> const &Di, REGRA const &producao, std::vector<std::vector<int>> &ponteiros) {
+	int marcador;
+	int p, p1, p2;
+	std::vector<int> ponteiro;
+
+	marcador = encontraMarcador(producao.cadeia_simbolos);
+	if (marcador - 1 >= 0 && (producao.cadeia_simbolos.size() == 3 || marcador + 1 < producao.cadeia_simbolos.size() - 1)) { // Se o marcador da producao estiver entre duas variaveis
+		p = encontraReducao(producao.cadeia_simbolos[marcador - 1], Dr, Di);
+		ponteiro.push_back(p);
+		ponteiros.push_back(ponteiro);
+	} else if (marcador == 2 && producao.cadeia_simbolos.size() == 4) {
+		std::string var1, var2;
+
+		marcador = encontraMarcador(producao.cadeia_simbolos);
+		var1 = producao.cadeia_simbolos[marcador - 2];
+		var2 = producao.cadeia_simbolos[marcador - 1];
+
+		p1 = encontraReducaoAnterior(var1, Dr, Di);
+
+		p2 = encontraReducao(var2, Dr, Di);
+
+		ponteiro.push_back(p1);
+		ponteiro.push_back(p2);
+		ponteiros.push_back(ponteiro);
+	}
+}
+
+// -----------------------------------------------------------------------------
+
 void constroiD0(GRAMATICA const &G, std::vector<REGRA> &D0) {
 	//std::vector<std::string> producaoAux;
 	D0.clear();
@@ -110,9 +189,15 @@ void constroiD0(GRAMATICA const &G, std::vector<REGRA> &D0) {
 
 // -----------------------------------------------------------------------------
 
-void constroiDr(GRAMATICA const &G, std::vector<std::vector<REGRA>> &Dr, std::vector<std::string> const &sentenca) {
+void constroiDr(GRAMATICA const &G, std::vector<std::vector<REGRA>> &Dr, std::vector<std::string> const &sentenca, std::vector<std::vector<int>> &ponteiros) {
 	int simboloMarcado;
 	std::vector<REGRA> Di;
+	std::vector<int> ponteiro;
+	ponteiro.push_back(-1);
+
+	for (int i = 0; i < Dr[0].size(); i++) {
+		ponteiros.push_back(ponteiro);
+	}
 
 	for (int i = 1; i <= sentenca.size(); i++) {
 		Di.clear();
@@ -121,6 +206,7 @@ void constroiDr(GRAMATICA const &G, std::vector<std::vector<REGRA>> &Dr, std::ve
 			simboloMarcado = encontraMarcador(Dr[i - 1][j].cadeia_simbolos) + 1;
 			if (Dr[i - 1][j].cadeia_simbolos[simboloMarcado].compare(sentenca[i - 1]) == 0) {
 				Di.push_back(Dr[i - 1][j]);
+				ponteiros.push_back(ponteiro);
 			}
 		}
 
@@ -140,6 +226,7 @@ void constroiDr(GRAMATICA const &G, std::vector<std::vector<REGRA>> &Dr, std::ve
 						avancaMarcador(regraAux.cadeia_simbolos, i);
 						if (!encontraProducao(regraAux, Di)) {
 							Di.push_back(regraAux);
+							ponteiros.push_back(ponteiro);
 						}
 					}
 				}
@@ -164,6 +251,7 @@ void constroiDr(GRAMATICA const &G, std::vector<std::vector<REGRA>> &Dr, std::ve
 							avancaMarcador(regraAux.cadeia_simbolos, -1);
 							if (!encontraProducao(regraAux, Di)) {
 								Di.push_back(regraAux);
+								encontraPonteiros(Dr, Di, regraAux, ponteiros);
 							}
 						}
 					}
@@ -193,24 +281,115 @@ bool aceitaSentenca(std::vector<REGRA> const &Dn, GRAMATICA const &G) {
 
 // -----------------------------------------------------------------------------
 
+void arvoreDeDerivacao(std::vector<std::vector<REGRA>> const &Dr, std::vector<std::vector<int>> &ponteiros, GRAMATICA const &G) {
+	int qtdRegras = 0;
+	int idRegra = 0;
+	int idRegraAux;
+	int simboloMarcado;
+	std::vector<REGRA> regras;
+	std::vector<int> idsRegras;
+	std::vector<std::string> terminais;
+
+	for (int i = 0; i < Dr.size(); i++)
+		for (int j = 0; j < Dr[i].size(); j++)
+			regras.push_back(Dr[i][j]);
+
+	for (int i = 0; i < Dr.size() - 1; i++)
+		for (int j = 0; j < Dr[i].size(); j++)
+			idRegra++;
+
+	for (int i = 0; i < Dr[Dr.size() - 1].size(); i++) {
+		simboloMarcado = encontraMarcador(Dr[Dr.size() - 1][i].cadeia_simbolos) + 1;
+		if (Dr[Dr.size() - 1][i].variavel.compare(G.inicial) == 0 && Dr[Dr.size() - 1][i].cadeia_simbolos[simboloMarcado].compare("/0") == 0) {
+			break;
+		} else {
+			idRegra++;
+		}
+	}
+	idsRegras.push_back(idRegra);
+	std::cout << regras[idsRegras[0]].variavel << "(S" << idsRegras[0] << ")\n";
+
+	int j = 0;
+	while (j < idsRegras.size()) {
+		int i = j;
+		int numRegras = idsRegras.size();
+
+		while (i < numRegras) {
+			if (ponteiros[idsRegras[i]].size() == 1) {
+				idRegraAux = ponteiros[idsRegras[i]][0];
+				if (idRegraAux != -1) {
+					std::cout << regras[idRegraAux].variavel << "(S" << idRegraAux << ") |";
+					idsRegras.push_back(idRegraAux);
+					if (encontraTerminal(regras[idRegraAux].cadeia_simbolos[0], G.terminais) && regras[idRegraAux].cadeia_simbolos.size() == 3)
+						terminais.push_back(regras[idRegraAux].cadeia_simbolos[0]);
+				} else {
+					std::cout << terminais[0] << "| ";
+					terminais.erase(terminais.begin() + 0);
+				}
+			} else {
+				idRegraAux = ponteiros[idsRegras[i]][0];
+				if (idRegraAux != -1) {
+					std::cout << regras[idRegraAux].variavel << "(S" << idRegraAux << ")    ";
+					idsRegras.push_back(idRegraAux);
+					if (encontraTerminal(regras[idRegraAux].cadeia_simbolos[0], G.terminais) && regras[idRegraAux].cadeia_simbolos.size() == 3)
+						terminais.push_back(regras[idRegraAux].cadeia_simbolos[0]);
+				} else {
+					std::cout << terminais[0] << "| ";
+					terminais.erase(terminais.begin() + 0);
+				}
+
+				idRegraAux = ponteiros[idsRegras[i]][1];
+				if (idRegraAux != -1) {
+					std::cout << regras[idRegraAux].variavel << "(S" << idRegraAux << ") |";
+					idsRegras.push_back(idRegraAux);
+					if (encontraTerminal(regras[idRegraAux].cadeia_simbolos[0], G.terminais) && regras[idRegraAux].cadeia_simbolos.size() == 3)
+						terminais.push_back(regras[idRegraAux].cadeia_simbolos[0]);
+				} else {
+					std::cout << terminais[0] << "| ";
+					terminais.erase(terminais.begin() + 0);
+				}
+			}
+			i++;
+			j++;
+		}
+		std::cout << "\n";
+	}
+
+
+	std::cout << "\n";
+}
+
+// -----------------------------------------------------------------------------
+
 void early(std::string const &entrada, GRAMATICA const &G) {
 	std::vector<REGRA> D0;
 	std::vector<std::vector < REGRA>> Dr;
 	std::vector<std::string> sentenca;
+	std::vector<std::vector<int>> ponteiros;
 
 	constroiD0(G, D0);
 	Dr.push_back(D0);
 
 	analisaSentenca(entrada, G, sentenca);
-	constroiDr(G, Dr, sentenca);
+	constroiDr(G, Dr, sentenca, ponteiros);
 
+	int s = 0;
+	int p = 0;
 	for (int k = 0; k < Dr.size(); k++) {
 		std::cout << "D" << k << "\n";
 		for (int i = 0; i < Dr[k].size(); i++) {
-			std::cout << Dr[k][i].variavel << " -> ";
+			std::cout << "S" << s << " " << Dr[k][i].variavel << " -> ";
 			for (int j = 0; j < Dr[k][i].cadeia_simbolos.size(); j++)
 				std::cout << "'" << Dr[k][i].cadeia_simbolos[j] << "' ";
+			if (ponteiros[p].size() == 1) {
+				std::cout << ponteiros[p][0];
+			} else {
+				for (int l = 0; l < ponteiros[p].size(); l++)
+					std::cout << ponteiros[p][l] << ", ";
+			}
 			std::cout << "\n";
+			s++;
+			p++;
 		}
 		std::cout << "\n";
 	}
@@ -221,6 +400,9 @@ void early(std::string const &entrada, GRAMATICA const &G) {
 	} else {
 		std::cout << "Sentenca rejeitada\n";
 	}
+
+	std::cout << "Arvore de Derivacao: \n";
+	arvoreDeDerivacao(Dr, ponteiros, G);
 }
 
 // -----------------------------------------------------------------------------
--- trabalhoFinal/early.h
+++ trabalhoFinal/early.h
@@ -16,15 +16,31 @@ void avancaMarcador(std::vector<std::string> &producao, int conjunto);
 
 // -----------------------------------------------------------------------------
 
+int encontraReducao(std::string const &variavel, std::vector<std::vector<REGRA>> const &Dr, std::vector<REGRA> const &Di);
+
+// -----------------------------------------------------------------------------
+
+int encontraReducaoAnterior(std::string const &variavel, std::vector<std::vector<REGRA>> const &Dr, std::vector<REGRA> const &Di);
+
+// -----------------------------------------------------------------------------
+
+void encontraPonteiros(std::vector<std::vector<REGRA>> const &Dr, std::vector<REGRA> const &Di, REGRA const &producao, std::vector<std::vector<int>> &ponteiros);
+
+// -----------------------------------------------------------------------------
+
 void constroiD0(GRAMATICA const &G, std::vector<REGRA> &D0);
 
 // -----------------------------------------------------------------------------
 
-void constroiDr(GRAMATICA const &G, std::vector<std::vector<REGRA>> &Dr, std::vector<std::string> const &sentenca);
+void constroiDr(GRAMATICA const &G, std::vector<std::vector<REGRA>> &Dr, std::vector<std::string> const &sentenca, std::vector<std::vector<int>> &ponteiros);
 
 // -----------------------------------------------------------------------------
 
-bool aceitaSentenca (std::vector<REGRA> const &Dn, GRAMATICA const &G);
+bool aceitaSentenca(std::vector<REGRA> const &Dn, GRAMATICA const &G);
+
+// -----------------------------------------------------------------------------
+
+void arvoreDeDerivacao(std::vector<std::vector<REGRA>> const &Dr, std::vector<std::vector<int>> const &ponteiros, GRAMATICA const &G);
 
 // -----------------------------------------------------------------------------
 
